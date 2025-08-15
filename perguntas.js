847a5afad081ed4bd93ab750a6bb34408e04ee92
OoFelipao <felipe.marcondes.barbosa@escola.pr.gov.br>
2025-08-15 11:42:37 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+  'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
